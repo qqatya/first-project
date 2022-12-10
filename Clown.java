60bad4644c2423e1352c0506d704879cab8418c8
katikatineboisya <buurgr@MacBook-Pro-Ekaterina.local>
2022-12-10 19:50:59 +0300

    Изменила метод makeHonk на clownMoment

--- Clown.java
+++ Clown.java
@@ -1,9 +1,9 @@
 public class Clown {
     public static void main(String[] args) {
-        makeHonk();
+        clownMoment();
     }
 
-    void makeHonk() {
-        System.out.println("Honk honk");
+    void clownMoment() {
+        System.out.println("You are such a clown");
     }
 }
