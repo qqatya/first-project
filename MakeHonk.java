e448601c180603d605110f0890865ae6c6984481
katikatineboisya <katikatineboisya@gmail.com>
2022-12-10 19:58:10 +0300

    Добавила метод makeHonk в нужный файл

---
+++ MakeHonk.java
@@ -0,0 +1,10 @@
+public class MakeHonk {
+    public static void main(String[] args) {
+        makeHonk();
+    }
+    
+    void makeHonk() {
+        System.out.println("Make honk great again");
+    }
+
+}
